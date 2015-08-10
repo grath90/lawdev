fc471843b3a08dd836eedb34c1199c23d231ee5b
Cole Rath <grath90@gmail.com>
2015-08-10 14:29:52 -0500

    changed title

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lassiter</title>
+    <title>Lassiter & Cassinelli</title>
     <!--<link href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="../vendor/components/jquery/jquery.min.js"></script>-->
     <!-- Latest compiled and minified CSS -->
@@ -22,7 +22,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ route('index') }}">Lassiter & Cassinelli</a>
+                <a class="navbar-brand" href="{{ route('index') }}">Lassiter</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
