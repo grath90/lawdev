94217038d691653098464c4f9c3e81292001c1b4
Cole Rath <grath90@gmail.com>
2015-08-20 12:59:43 -0500

    Changed wording on index and lassiter. Requested by client.
    Changes are as follows:
    
    On Main Page:
    
    1st paragraph: "Mr Lassiter has been licensed for thirty-seven
    years..." MAKE THAT FORTY TWO YEARS
    
    2nd paragraph: ...."and has worked under Mr. Lassiter and Ms.
    Cassinelli an associate attorney..." NEEDS TO SAY AS AN
    
    On Jack's Bio Page:
    
    1st paragraph Second Line: "...in criminal trials and appeals for
    thirty" CHANGE TO THIRTY EIGHT
    
    3rd paragraph First Line: "In November of 2011..." CHANGE THE FIRST
    PART OF THAT FIRST SENTENCE TO "IN 2012, MR. LASSITER WAS ACCEPTED AS A
    FELLOW IN THE..."
    
    3rd paragraph Second Line: Remove "In December, 2009" AND SAY "HE WAS
    APPOINTED AND SERVED AS CHAIR TO THE GOVERNOR'S TASK FORCE ON RACIAL
    PROFILING FROM 2008 TO 2010."
    
    3rd paragraph 6th line: "For 26 years, Jack served..." CHANGE TO "HE
    SERVED..."
    
    3rd paragraph 7th line: ADD "...on the Arkansas Crime Information
    Center Supervisory Board FOR OVER THIRTY YEARS, and ..."

--- resources/views/attorneys/lassiter.blade.php
+++ resources/views/attorneys/lassiter.blade.php
@@ -1,7 +1,7 @@
 @extends("templates.attorneyTemplate")
 @section("bio")
                 <p>Jack Lassiter is a criminal defense lawyer who practices in both state and federal courts.
-                    He has appeared twice before the United States Supreme Court and has represented the defense in criminal trials and appeals for thirty years.
+                    He has appeared twice before the United States Supreme Court and has represented the defense in criminal trials and appeals for thirty-eight years.
                     He graduated from Southwestern at Memphis, now Rhodes College, in 1969 with a B.A. in Political Science.
                     After teaching junior high kids for a year, he entered the University of Arkansas at Fayetteville, School of Law where he graduated in 1973.
                     Following law school he clerked a year for the Arkansas Supreme Court, and then worked as a trial lawyer in the criminal division of the Arkansas Attorney General’s office.
@@ -11,12 +11,12 @@
                     He was on the defense team that won the second Whitewater trial,
                     which was the first defeat of any independent counsel at trial under the now defunct Independent Counsel Act.</p>
                     <!--<br>-->
-                <p>In November of 2011,  Mr. Lassiter was notified of his nomination for fellowship in the American College of Trial Lawyers.
-                    In December, 2009 he was appointed, and serves as chair to the Governor’s Task Force on Racial Profiling. In January,
+                <p>In 2012, Mr. Lassiter was accepted as a fellow in the American College of Trial Lawyers.
+                    He was appointed and served as chair to the Governor's Task Force on Racial Profiling from 2008 to 2010. In January,
                     2009 he was appointed to the Arkansas Supreme Court Committee on Criminal Practice and served until 2014.
                     He served as the chair of the Arkansas Bar Association’s Task Force Committee on Criminal Sentencing Statutes.
                     He was the Association’s representative on the Criminal Code Revision Commission in 2003-2004.
-                    For 26 years, Jack served as the Arkansas Bar Association’s representative on the Arkansas Crime Information Center Supervisory Board,
+                    For 26 years, he served as the Arkansas Bar Association’s representative on the Arkansas Crime Information Center Supervisory Board for over thirty years,
                     and he served as chair from 1993 to 2014.
                     He has been a member of the Association’s Criminal Law Section for many years, serving as its chair in 1982-83.
                     He served on the Arkansas Supreme Court Committee on Model Criminal Jury Instructions in 1990-2000.
