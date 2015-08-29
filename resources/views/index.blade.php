10fe5b879c98c86122932bd8910df17d7a2c9981
Cole Rath <grath90@gmail.com>
2015-08-29 00:39:12 -0500

    made more changes to the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,7 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
                 <h4>Jack Lassiter, Erin Cassinelli, and Lauren Collins are attorneys in Little Rock, Arkansas with a practice that focuses on criminal defense in federal and state courts throughout Arkansas.</h4>
             </div>
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
@@ -24,11 +24,11 @@
                     The College also counts the Justices of the United States Supreme Court among its membership.
                     Mr. Lassiter and Ms. Cassinelli are AV rated by Martindale-Hubbell, a rating indicating that their peers rank them at the highest level of professional excellence.</p>
                 <p>Lauren Collins began working for Mr. Lassiter and Ms. Cassinelli in early 2012 as a law clerk and has worked under Mr. Lassiter and Ms. Cassinelli as an associate attorney since early 2014.</p>
-                <p>Our legal assistant, Jené Louviere, is also available each day to answer client calls. She is happy to assist clients in making appointments, obtaining other information, and to generally provide assistance to those who contact our office.</p>
+                <p>Their legal assistant, Jené Louviere, is also available each day to answer client calls. She is happy to assist clients in making appointments, obtaining other information, and to generally provide assistance to those who contact our office.</p>
                 <p>The attorneys and staff at Lassiter & Cassinelli work diligently every day to ensure that each person is treated fairly and justly under the law and to provide outstanding representation to each one of their clients.</p>
-                <p>The staff at Lassiter & Cassinelli feel their relationships with their clients are of the utmost importance.
-                    Lassiter & Cassienlli chose this area of the profession because of their steadfast belief that each person suspected of a crime is entitled to and deserves effective representation.
-                    It is important to that each client be respected, feel comfortable sharing a difficult time in life with the staff, and know that they are proud to stand with them and defend them to the fullest extent.
+                <p>The attorneys at Lassiter & Cassinelli feel their relationships with their clients are of the utmost importance.
+                    They chose this area of the profession because of their steadfast belief that each person suspected of a crime is entitled to and deserves effective representation.
+                    It is important to the attorneys of Lassiter & Cassinelli that each client be respected, feel comfortable sharing a difficult time in life with the them, and know that they are proud to stand with them and defend them to the fullest extent.
                     The criminal justice system cannot function properly and achieve just results without adequate defensive representation and adversarial testing performed by an attorney who is committed to both the system and each client Lassiter & Cassinelli represents.</p>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
